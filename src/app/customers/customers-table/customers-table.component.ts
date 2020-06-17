8b2fab527b97fbb7071676b20984f204a2e57f1d
nemanjasoc <nemanjasoc@gmail.com>
2020-06-17 23:05:26 +0200

    customers table

--- src/app/customers/customers-table/customers-table.component.ts
+++ src/app/customers/customers-table/customers-table.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { LocalStorageService } from 'src/app/service/local-storage.service';
+import { Customer } from 'src/app/models/customer.model';
+import { MatDialog } from '@angular/material/dialog';
+import { CustomerEditDialogComponent } from 'src/app/customer/customer-edit/customer-edit-dialog.component';
+import { CommunicationService } from 'src/app/service/communication.service';
 
 @Component({
   selector: 'app-customers-table',
@@ -6,10 +11,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./customers-table.component.scss']
 })
 export class CustomersTableComponent implements OnInit {
+  customers: Customer[] = [];
 
-  constructor() { }
+
+  constructor(public dialog: MatDialog, 
+    public communicationService: CommunicationService,
+    private lsService: LocalStorageService) { }
+
 
   ngOnInit(): void {
+    this.customers = this.lsService.getCustomers();
+  }
+
+  openDialog() {
+    this.dialog.open(CustomerEditDialogComponent, {
+      height: '600px',
+      width: '600px'
+    }).afterClosed().subscribe(() => {
+      this.customers = this.lsService.getCustomers();
+    });
+    this.communicationService.editCustomerIsOpen = false;
   }
 
 }
